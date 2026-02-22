433c8023fd5ea241bfe49fb2c68b08f068f7688e
Sumit Chauhan <r.cgamerz2944@gmail.com>
2026-02-22 13:31:55 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
-const API_BASE = "http://localhost:5000";
+const API_BASE =
+  process.env.REACT_APP_API_URL || "http://localhost:5000";
 
 export async function testBackend() {
-  const res = await fetch(API_BASE + "/");
+  const res = await fetch(`${API_BASE}/`);
   return res.text();
 }
